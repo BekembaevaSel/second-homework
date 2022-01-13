621499701cbc0c95ec28bef6d56078815a9f9817
BekembaevaSel <bekembaeva8@gmail.com>
2022-01-13 23:21:41 +0600

    i created component with 3 components in one.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Facts from './components/Facts'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const descriptions = {
+	cityImage:
+		'https://avatars.mds.yandex.net/get-zen_doc/48747/pub_5ab33f0ac3321babbd43a40d_5ab33f81bcf1bcb6689dc637/scale_1200',
+
+	city: 'New-york',
+	currentTime: new Date(),
+	mainInformation: {
+		population: '8,419 millions',
+		showplace: 'statue of Liberty',
+	},
 }
 
-export default App;
+function App() {
+	return (
+		<div className='App'>
+			<Facts
+				image={descriptions.cityImage}
+				city={descriptions.city}
+				currentTime={descriptions.currentTime}
+				mainInformation={descriptions.mainInformation}
+			/>
+		</div>
+	)
+}
+
+export default App
